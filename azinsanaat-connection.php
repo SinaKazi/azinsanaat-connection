a1ef3e1a0697d2084066f2864cac661c99251df3
SinaKazi <dr.tintin92@gmail.com>
2025-11-19 14:03:45 +0330

    1.8.13

--- azinsanaat-connection.php
+++ azinsanaat-connection.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Azinsanaat Connection
  * Description: اتصال به آذین صنعت و همگام‌سازی محصولات از طریق API ووکامرس.
- * Version:     1.8.12
+ * Version:     1.8.13
  * Author:      Sina Kazemi
  * Requires PHP: 8.1
  */
